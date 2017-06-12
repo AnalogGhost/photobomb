6b04dc9f4f3278d5e54d7b3bfc4ef60deb6ae100
Mathew Brown <matt@mattbrown.com>
2017-06-12 15:25:15 -0600

    Add single photo route with optional parameter.

--- routes/photos.js
+++ routes/photos.js
@@ -3,14 +3,7 @@ const router = express.Router();
 
 const queries = require('../queries/photos');
 
-router.get('/', (req,res,next) => {
-  queries.get()
-  .then(result => {
-    res.send(result);
-  });
-});
-
-router.get('/:id', (req,res,next) => {
+router.get('/:id?', (req,res,next) => {
   queries.get(req.params.id)
   .then(result => {
     res.send(result);
